8433ee923c814c5b638f5e2ce5573b2a00506193
aubeOSG <caube@osg.ca>
2021-12-07 15:47:03 -0500

    INIT - parcel / react / css modules boilerplate

---
+++ src/components/_app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as styles from './styles.module.scss'
+
+const Page = () => {
+    return (
+        <div className={styles.container}>Hello World</div>
+    )
+}
+
+export default Page
