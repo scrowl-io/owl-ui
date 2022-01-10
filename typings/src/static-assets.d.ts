51c796db964fc8fcb57afc1192d75fd33d91592e
aubeOSG <caube@osg.ca>
2022-01-10 10:23:56 -0500

    Update static-assets.d.ts

--- typings/src/static-assets.d.ts
+++ typings/src/static-assets.d.ts
@@ -2,32 +2,32 @@
  * Global ambient declaration of various non javascript assets being imported into JS/TS
  */
 
- declare module '*.svg' {
+declare module '*.svg' {
     const src: string;
     export default src;
-  }
+}
   
-  declare module '*.gif' {
+declare module '*.gif' {
     const src: string;
     export default src;
-  }
+}
   
-  declare module '*.jpg' {
+declare module '*.jpg' {
     const src: string;
     export default src;
-  }
+}
   
-  declare module '*.jpeg' {
+declare module '*.jpeg' {
     const src: string;
     export default src;
-  }
+}
   
-  declare module '*.png' {
+declare module '*.png' {
     const src: string;
     export default src;
-  }
-  
-  declare module '*.md' {
+}
+
+declare module '*.md' {
     const src: string;
     export default src;
-  }
+}
