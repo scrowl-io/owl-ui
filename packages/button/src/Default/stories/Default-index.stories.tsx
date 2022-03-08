ad3662c37854b6f1485cb92f6e29d8b45387d23a
aubeOSG <caube@osg.ca>
2022-03-08 14:24:50 -0500

    UPDATE [sc-243] - added component props to stories

--- packages/button/src/Default/stories/Default-index.stories.tsx
+++ packages/button/src/Default/stories/Default-index.stories.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { component as Button } from '../index'
+import { DefaultButtonProps } from '../Default.types'
 
 import { appearance } from './Default-appearance.stories'
 import { size } from './Default-size.stories'
 import { theme } from './Default-theme.stories'
 
-export const Default = (args) => <Button {...args}>Hello World</Button>
+export const Default = (args: DefaultButtonProps) => <Button {...args}><>Hello World</></Button>
 
 Default.args = {
     appearance: 'Primary',
