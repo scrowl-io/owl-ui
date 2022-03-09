ba5b26e108da6de10961948511af5607d283bb78
aubeOSG <caube@osg.ca>
2022-03-09 14:02:52 -0500

    UPDATE [sc-243] - change children prop to generic type

--- packages/button/src/Default/Default.types.ts
+++ packages/button/src/Default/Default.types.ts
@@ -1,9 +1,11 @@
+import React from 'react'
+
 export type DefaultButtonCommons = {
     appearance?: 'Primary' | 'Outline'
     theme?: 'Default' | 'Dark'
     size?: 'Sm' | 'Md' | 'Lg'
     prefix?: string
-    children: JSX.Element | JSX.Element[]
+    children: React.ReactNode
 }
 
 export type DefaultButtonProps = Partial<DefaultButtonCommons>
--- packages/textfield/src/Default/Default.types.ts
+++ packages/textfield/src/Default/Default.types.ts
@@ -1,9 +1,11 @@
+import React from 'react'
+
 export type DefaultTextfieldCommons = {
     appearance?: 'Primary' | 'Outline'
     theme?: 'Default' | 'Dark'
     size?: 'Sm' | 'Md' | 'Lg'
     prefix?: string
-    children: JSX.Element | JSX.Element[]
+    children: React.ReactNode
 }
 
 export type DefaultTextfieldProps = Partial<DefaultTextfieldCommons>
