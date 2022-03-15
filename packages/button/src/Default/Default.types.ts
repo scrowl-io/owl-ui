19719e41eceaffc7dfe993ac009aae2a6199329f
Cicero Fonseca <cicero@osg.ca>
2022-03-15 16:44:49 -0400

    Update types and appearence values

--- packages/button/src/Default/Default.types.ts
+++ packages/button/src/Default/Default.types.ts
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export type ButtonDefaultCommons = {
-  appearance?: 'Primary' | 'Outline';
-  theme?: 'Default' | 'Dark';
-  size?: 'Sm' | 'Md' | 'Lg';
-  prefix?: string;
+  appearance?: 'Default' | 'Primary' | 'Link';
   children: React.ReactNode;
+  onClick: () => void;
+  prefix?: string;
+  size?: 'Sm' | 'Md' | 'Lg';
+  theme?: 'Default' | 'Dark';
 };
 
 export type ButtonDefaultProps = Partial<ButtonDefaultCommons> &
