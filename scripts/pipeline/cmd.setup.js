35ea54ba06d2f3e52135bc0076333b3aa17b5e46
aubeOSG <caube@osg.ca>
2022-03-15 15:05:33 -0400

    UPDATE [sc-364] - linting

--- scripts/pipeline/cmd.setup.js
+++ scripts/pipeline/cmd.setup.js
@@ -1,26 +1,28 @@
-import fs from '../utls/file-system.js'
+import fs from '../utls/file-system.js';
 
 const fileMap = {
-    'design/dist/styledictionary/scss/variables.scss': {
-        dest: 'packages/theme/src/global/_variables.scss',
-        transformer: (contents) => {
-            return contents.replace(/;/g, () => { return ' !default;' })
-        }
-    }
-}
+  'design/dist/styledictionary/scss/variables.scss': {
+    dest: 'packages/theme/src/global/_variables.scss',
+    transformer: raw => {
+      const contents = raw.replace(/;/g, () => {
+        return ' !default;';
+      });
+      return contents;
+    },
+  },
+};
 
 function copy() {
-  
-    for (let file in fileMap) {
-        let contents = fs.getFile(file)
-        const dest = fileMap[file].dest
-        
-        if (fileMap[file].transformer) {
-            contents = fileMap[file].transformer(contents)
-        }
-        
-        fs.setFile(dest, contents)
+  for (let file in fileMap) {
+    let contents = fs.getFile(file);
+    const dest = fileMap[file].dest;
+
+    if (fileMap[file].transformer) {
+      contents = fileMap[file].transformer(contents);
     }
+
+    fs.setFile(dest, contents);
+  }
 }
 
-copy()
+copy();
