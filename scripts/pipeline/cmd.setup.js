9f5216fcf0e1b6f2f61bdaf3f5b034488ba5b373
aubeOSG <caube@osg.ca>
2022-03-15 16:00:12 -0400

    UPDATE [sc-364] - applied px to rem conversion

--- scripts/pipeline/cmd.setup.js
+++ scripts/pipeline/cmd.setup.js
@@ -1,12 +1,28 @@
 import fs from '../utls/file-system.js';
 
+const defaultUnit = 16;
 const fileMap = {
   'design/dist/styledictionary/scss/variables.scss': {
     dest: 'packages/theme/src/global/_variables.scss',
     transformer: raw => {
-      const contents = raw.replace(/;/g, () => {
-        return ' !default;';
-      });
+      const contents = raw
+        .replace(
+          /(\$(?:[\w]+-)+typeface(?:-[\w]+)+:) (\d+)px;\n/g,
+          (line, token, unit) => {
+            const size = unit / defaultUnit;
+
+            return `${token} ${size}rem;\n`;
+          }
+        )
+        .replace(/(\$(?:[\w]+-)+spacer:) (\d+)px;/g, (line, token, unit) => {
+          const size = unit / defaultUnit;
+
+          return `${token} ${size}rem;\n`;
+        })
+        .replace(/;/g, () => {
+          return ' !default;';
+        });
+
       return contents;
     },
   },
