b157f6cfe2bd0b23b87ed7fae810975c71a7fd5f
Cicero Fonseca <cicero@osg.ca>
2022-03-16 14:18:39 -0400

    Update button styles

--- packages/button/src/Default/Default.types.ts
+++ packages/button/src/Default/Default.types.ts
@@ -1,15 +1,18 @@
 import React from 'react';
 
 export type ButtonDefaultCommons = {
-  appearance?: 'Default' | 'Primary';
+  appearance?: 'Default' | 'Primary' | 'Link';
   ariadescribedby?: string;
   arialabel?: string;
+  onClick?: () => void;
   children: React.ReactNode;
   disabled?: boolean;
+  href?: string;
   prefix?: string;
   size?: 'Sm' | 'Md' | 'Lg';
   theme?: 'Default' | 'Dark';
 };
 
 export type ButtonDefaultProps = Partial<ButtonDefaultCommons> &
-  React.ButtonHTMLAttributes<HTMLButtonElement>;
+  React.ButtonHTMLAttributes<HTMLButtonElement> &
+  React.AnchorHTMLAttributes<HTMLAnchorElement>;
