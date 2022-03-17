6b585fefed2192e05d5b4cf46e933d6a3db332f4
Cicero Fonseca <cicero@osg.ca>
2022-03-17 13:53:27 -0400

    Update default component story

--- packages/card/src/Default/stories/Default-index.stories.tsx
+++ packages/card/src/Default/stories/Default-index.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { component as Card } from '../index';
 import { component as CardHeader } from '../../CardHeader/index';
+import { component as CardBody } from '../../CardBody/index';
 import { CardDefaultProps } from '../Default.types';
 
 import { appearance } from './Default-appearance.stories';
@@ -9,8 +10,11 @@ import { theme } from './Default-theme.stories';
 export const Default = (args: CardDefaultProps) => (
   <Card {...args}>
     <CardHeader>
-      <h3>Card Header</h3>
+      <h3>Card Title</h3>
     </CardHeader>
+    <CardBody>
+      <p>Some quick example text to build on the card content</p>
+    </CardBody>
   </Card>
 );
 
