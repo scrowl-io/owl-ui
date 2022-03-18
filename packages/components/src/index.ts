d414a3f4f35badc46452392df4f9d2d9ddcc629b
aubeOSG <caube@osg.ca>
2022-03-18 14:27:06 -0400

    LINTER

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,13 +1,13 @@
-import * as Button from '@owlui/button'
-import * as Card from '@owlui/card'
-import * as Textfield from '@owlui/textfield'
+import * as Button from '@owlui/button';
+import * as Card from '@owlui/card';
+import * as Textfield from '@owlui/textfield';
 
-export const button = Button
-export const card = Card
-export const textfield = Textfield
+export const button = Button;
+export const card = Card;
+export const textfield = Textfield;
 
 export default {
-    button,
-    card,
-    textfield,
-}
+  button,
+  card,
+  textfield,
+};
