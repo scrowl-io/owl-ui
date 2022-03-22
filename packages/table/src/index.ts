b5b755d150b9490c5106ce48f904678b5b240fe9
Cicero Fonseca <cicero@osg.ca>
2022-03-22 09:29:44 -0400

    Linting

--- packages/table/src/index.ts
+++ packages/table/src/index.ts
@@ -1,29 +1,29 @@
-import * as BodyTable from './Body/index'
-import * as CellTable from './Cell/index'
-import * as DefaultTable from './Default/index'
-import * as HeaderTable from './Header/index'
-import * as RowTable from './Row/index'
+import * as BodyTable from './Body/index';
+import * as CellTable from './Cell/index';
+import * as DefaultTable from './Default/index';
+import * as HeaderTable from './Header/index';
+import * as RowTable from './Row/index';
 
-export const Body = BodyTable.component
-export const BodyTypes = BodyTable.types
-export const Cell = CellTable.component
-export const CellTypes = CellTable.types
-export const Default = DefaultTable.component
-export const DefaultTypes = DefaultTable.types
-export const Header = HeaderTable.component
-export const HeaderTypes = HeaderTable.types
-export const Row = RowTable.component
-export const RowTypes = RowTable.types
+export const Body = BodyTable.component;
+export const BodyTypes = BodyTable.types;
+export const Cell = CellTable.component;
+export const CellTypes = CellTable.types;
+export const Default = DefaultTable.component;
+export const DefaultTypes = DefaultTable.types;
+export const Header = HeaderTable.component;
+export const HeaderTypes = HeaderTable.types;
+export const Row = RowTable.component;
+export const RowTypes = RowTable.types;
 
 export default {
-    Body,
-    BodyTypes,
-    Cell,
-    CellTypes,
-    Default,
-    DefaultTypes,
-    Header,
-    HeaderTypes,
-    Row,
-    RowTypes,
-}
+  Body,
+  BodyTypes,
+  Cell,
+  CellTypes,
+  Default,
+  DefaultTypes,
+  Header,
+  HeaderTypes,
+  Row,
+  RowTypes,
+};
