888fe0d9e800b006bfc9e06c282eab269a544d5a
Cicero Fonseca <cicero@osg.ca>
2022-03-24 11:37:47 -0400

    Clean up imports and run prettier/eslint fixes

--- packages/table/src/index.ts
+++ packages/table/src/index.ts
@@ -1,24 +1,9 @@
-import * as BodyTable from './Body/index';
-import * as CellTable from './Cell/index';
 import * as DefaultTable from './Default/index';
-import * as RowTable from './Row/index';
 
-export const Body = BodyTable.component;
-export const BodyTypes = BodyTable.types;
-export const Cell = CellTable.component;
-export const CellTypes = CellTable.types;
 export const Default = DefaultTable.component;
 export const DefaultTypes = DefaultTable.types;
-export const Row = RowTable.component;
-export const RowTypes = RowTable.types;
 
 export default {
-  Body,
-  BodyTypes,
-  Cell,
-  CellTypes,
   Default,
   DefaultTypes,
-  Row,
-  RowTypes,
 };
