eda694ac67151490c98e190b39e4a0636f914ce9
aubeOSG <caube@osg.ca>
2022-03-28 14:03:30 -0400

    FIX [sc-393] import error during build
    @parcel/transformer-typescript-types: Module '"@owlui/utils"' has no exported member 'getCssModClass'. Did you mean to use 'import getCssModClass from "@owlui/utils"' instead?

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,10 +1,2 @@
-import * as Lib from './utils';
-import * as Types from './types';
-
-export const lib = Lib;
-export const types = Types;
-
-export default {
-  lib,
-  types,
-};
+export * from './utils';
+export * from './types';
