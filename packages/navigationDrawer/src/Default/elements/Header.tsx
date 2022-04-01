3a6832301d2a73a23ecd305e9bd9536bf9d50e21
aubeOSG <caube@osg.ca>
2022-04-01 11:21:40 -0400

    UPDATE [sc-302] - removed unnecessary code

--- packages/navigationDrawer/src/Default/elements/Header.tsx
+++ packages/navigationDrawer/src/Default/elements/Header.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import { NavigationDrawerElementProps } from '../Default.types';
 import * as styles from '../styles.module.scss';
-import { getCssModClass, cleanCopy } from '@owlui/utils';
 
 const baseClass = 'owluiNavigationDrawerHeader';
 
 export const Component = (props: NavigationDrawerElementProps) => {
   const { children } = props;
-  const localProps: NavigationDrawerElementProps = cleanCopy(props);
 
-  localProps.className = getCssModClass({ localProps, baseClass, styles });
-  return <div className={localProps.className}>{children}</div>;
+  return <div className={styles[baseClass]}>{children}</div>;
 };
 
 export default {
