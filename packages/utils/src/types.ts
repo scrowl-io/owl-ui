a403c4bee899adaba05d36a2e72cdaf45ce766b6
cicerofonsecaosg <cicero@osg.ca>
2022-04-21 13:19:10 -0400

    [sc-621] - Add util createLocalProps function and types

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -9,3 +9,27 @@ export type CSSModClass = {
   styles: stylesProp;
   baseClass: string;
 };
+
+export type localProps = {
+  // [key: string]: unknown;
+  props: object;
+  styles: {
+    module: stylesProp;
+    classes: {
+      base: string;
+      optionals: Array<{ fields: string[]; value: string }>;
+    };
+  };
+  removables: string[];
+};
+
+export type createLocalPropsProps = Record<string, string>;
+
+export type createStyles = {
+  module: stylesProp;
+  classes: {
+    base: string;
+    prefix: string;
+    optionals: Array<{ fields: string[]; value: string }>;
+  };
+};
