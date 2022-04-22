a9e5ec67b7559a3e8776a97c8f5ba0c1a158c342
cicerofonsecaosg <cicero@osg.ca>
2022-04-22 13:24:22 -0400

    [sc-621] - Update button component and types

--- packages/button/src/Default/Default.tsx
+++ packages/button/src/Default/Default.tsx
@@ -1,62 +1,43 @@
 import * as React from 'react';
 import { ButtonDefaultProps } from './Default.types';
 import * as styleMod from './styles.module.scss';
-
-const baseClass = 'button';
+import { createLocalProps } from '@owlui/utils';
 
 export const Component = (props: ButtonDefaultProps) => {
-  const {
-    children,
-    className,
-    style,
-    arialabel,
-    ariadescribedby,
-    onClick,
-    href,
-  } = props;
+  const baseClass = 'button';
+  const { href, children } = props;
+  const prefix = props.prefix || '';
 
-  const modulePrefix = props.prefix;
-  const theme = props.theme || 'Default';
-  const appearance = props.appearance || 'Default';
-  const size = props.size || 'Md';
-  const styleLocal = {
-    base: styleMod[baseClass],
-    theme: styleMod[`${baseClass}Theme${theme}`],
-    appearance: styleMod[`${baseClass}Theme${theme}${appearance}`],
-    size: styleMod[`${baseClass}Size${size}`],
-  };
-
-  if (modulePrefix !== undefined && modulePrefix !== null) {
-    styleLocal.base = `${modulePrefix}-${styleLocal.base}`;
-    styleLocal.theme = `${modulePrefix}-${styleLocal.theme}`;
-    styleLocal.appearance = `${modulePrefix}-${styleLocal.appearance}`;
-    styleLocal.size = `${modulePrefix}-${styleLocal.size}`;
-  }
+  const locals = createLocalProps(
+    props,
+    {
+      module: styleMod,
+      classes: {
+        base: baseClass,
+        prefix: prefix,
+        optionals: [
+          {
+            fields: ['theme'],
+            value: 'Theme',
+          },
+          {
+            fields: ['theme', 'appearance'],
+            value: 'Theme',
+          },
+          {
+            fields: ['size'],
+            value: 'Size',
+          },
+        ],
+      },
+    },
+    ['prefix', 'theme', 'appearance', 'size']
+  );
 
   const buttonRenderer = () => {
     const Element = href ? 'a' : 'button';
-    const elementClass = [
-      className,
-      styleLocal.base,
-      styleLocal.theme,
-      styleLocal.appearance,
-      styleLocal.size,
-    ]
-      .join(' ')
-      .trim();
 
-    return (
-      <Element
-        href={href}
-        onClick={onClick}
-        style={style}
-        className={elementClass}
-        aria-label={arialabel}
-        aria-describedby={ariadescribedby}
-      >
-        {children}
-      </Element>
-    );
+    return <Element {...locals}>{children}</Element>;
   };
   return <>{buttonRenderer()}</>;
 };
