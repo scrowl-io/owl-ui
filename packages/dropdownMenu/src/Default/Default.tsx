554fa59f3ce78e549f970734358b577eeba6c4e1
Sean Clarke <seanclarke@Seans-MacBook-Pro.local>
2022-05-19 15:46:19 -0400

    add arrow key functionality

--- packages/dropdownMenu/src/Default/Default.tsx
+++ packages/dropdownMenu/src/Default/Default.tsx
@@ -16,7 +16,7 @@ export const Component = (props: DropdownMenuDefaultProps) => {
   const [selectedItem, setSelectedItem] = React.useState<string>('');
   const [dropdownOpen, setDropdownOpen] = React.useState(false);
 
-  dropdown ? (dropdown.selectedItem = selectedItem) : '';
+  // dropdown ? (dropdown.selectedItem = selectedItem) : '';
 
   const locals = createLocalProps(
     props,
@@ -44,14 +44,28 @@ export const Component = (props: DropdownMenuDefaultProps) => {
     ['prefix', 'theme', 'appearance', 'size']
   );
 
-  const toggleDropdown = () => {
-    dropdownOpen ? setDropdownOpen(false) : setDropdownOpen(true);
+  const toggleDropdown = (e) => {
+    if (dropdownOpen) {
+      setDropdownOpen(false);
+    } else if (e && e.type === 'click') {
+      setDropdownOpen(true);
+    } else {
+      setDropdownOpen(true);
+      const firstLi = document.querySelector('#list-item-1');
+      selectWithArrows(firstLi);
+    }
     const dropdown = document.querySelector('#dropdown-wrapper');
     dropdown?.classList.toggle('owlui-dropdown-menuwrapperHidden');
+    if (selectedItem) {
+      console.log(selectedItem);
+      const idNum = stripString(selectedItem);
+      const targetLi = document.querySelector(`#list-item-${idNum}`);
+      targetLi?.classList.add('owlui-dropdown-menu-selectedMenuItem');
+      selectWithArrows(targetLi);
+    }
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const selectItem = (e: { target: any }) => {
+  const selectItem = (e) => {
     const previouslySelected = document.querySelector(
       '.owlui-dropdown-menu-selectedMenuItem'
     );
@@ -63,39 +77,66 @@ export const Component = (props: DropdownMenuDefaultProps) => {
     setSelectedItem(selectedItem.innerText);
   };
 
-  const selectWithArrows = target => {
+  const selectWithArrows = (target: HTMLElement | null) => {
     const previouslySelected = document.querySelector(
       '.owlui-dropdown-menu-selectedMenuItem'
     );
     previouslySelected?.classList.remove(
       'owlui-dropdown-menu-selectedMenuItem'
     );
-    const selectedItem = target;
-    selectedItem.classList.add('owlui-dropdown-menu-selectedMenuItem');
-    setSelectedItem(selectedItem.innerText);
+    if (target) {
+      const selectedItem = target;
+      selectedItem.classList.add('owlui-dropdown-menu-selectedMenuItem');
+      selectedItem.scrollIntoView(false);
+      setSelectedItem(selectedItem.innerText);
+    } else if (selectedItem) {
+      const idNum = stripString(selectedItem);
+      const targetLi = document.querySelector(`#list-item-${idNum}`);
+      targetLi?.classList.add('owlui-dropdown-menu-selectedMenuItem');
+    } else {
+      const firstLi = document.querySelector('#list-item-1');
+      firstLi?.classList.add('owlui-dropdown-menu-selectedMenuItem');
+      firstLi?.scrollIntoView(false);
+      setSelectedItem(firstLi?.innerText);
+    }
   };
 
-  const stripString = (string) => {
-    const thenum = string.replace(/^\D+/g, '');
-    return thenum;
+  const stripString = (string: string) => {
+    const idNum = string.replace(/^\D+/g, '');
+    return idNum;
   };
 
-  const handleKeypress = e => {
+  const handleKeypress = (e: KeyboardEvent) => {
     switch (e.code) {
       case 'Enter':
         selectItem(e);
         break;
       case 'Escape':
-        toggleDropdown();
+        toggleDropdown(e);
         break;
       case 'ArrowDown':
+        // const selectedItem = document.querySelector('.owlui-dropdown-menu-selectedMenuItem');
         if (e.target.id === 'select-button' && !dropdownOpen) {
-          toggleDropdown();
-          const firstLi = document.querySelector('#list-item-1');
-          selectWithArrows(firstLi);
-        } else if (dropdownOpen) {
-          const currentlySelected = document.querySelector('.owlui-dropdown-menu-selectedMenuItem');
+          if (selectedItem) {
+            toggleDropdown(e);
+            const idNum = stripString(selectedItem);
+            const target = document.querySelector(`#list-item-${idNum}`);
+            selectWithArrows(target);
+          } else {
+            toggleDropdown(e);
+            const firstLi = document.querySelector('#list-item-1');
+            selectWithArrows(firstLi);
+          }
+        } else if (
+          document.querySelector('.owlui-dropdown-menu-selectedMenuItem')
+        ) {
+          const currentlySelected = document.querySelector(
+            '.owlui-dropdown-menu-selectedMenuItem'
+          );
           const prevId = stripString(currentlySelected.id);
+          if (prevId === dropdown?.items.length) {
+            return;
+          }
           const targetId = parseInt(prevId) + 1;
           const target = document.querySelector(`#list-item-${targetId}`);
 
@@ -103,25 +144,52 @@ export const Component = (props: DropdownMenuDefaultProps) => {
         }
         break;
       case 'ArrowUp':
-        console.log('up');
+        if (dropdownOpen) {
+          const currentlySelected = document.querySelector(
+            '.owlui-dropdown-menu-selectedMenuItem'
+          );
+          const prevId = stripString(currentlySelected.id);
+          const targetId = parseInt(prevId) - 1;
+          const target = document.querySelector(`#list-item-${targetId}`);
+
+          selectWithArrows(target);
+        }
         break;
     }
   };
+
+  // const ref = React.useRef();
+
+  // React.useEffect(() => {
+  //   const checkClickedOutside = e => {
+  //     if(dropdownOpen && ref.current && !ref.current.contains(e.target)){
+  //       console.log(document.querySelector('.owlui-dropdown-menu-selectedMenuItem').innerText);
+  //       const selection = (document.querySelector('.owlui-dropdown-menu-selectedMenuItem').innerText)
+  //       setSelectedItem(selection);
+  //       console.log(selectedItem);
+  //       setDropdownOpen(false);
+  //       toggleDropdown(e);
+  //     }
+  //   };
+  //   document.addEventListener('mousedown', checkClickedOutside);
+  //   return () => {
+  //     document.removeEventListener('mousedown', checkClickedOutside);
+  //   };
+  // }, [dropdownOpen]);
 
   const dropdownWrapperClass = `${baseClass}wrapperHidden`;
 
   return (
     <div {...locals}>
-      <div onKeyDown={handleKeypress}>{dropdown && dropdown['label']}</div>
-      <button id="select-button" onKeyDown={handleKeypress} onClick={toggleDropdown}>
-        <span>{selectedItem ? selectedItem : 'Item 1'}</span>
-        {/* <span className="icon">&#8595;</span> */}
-      </button>
-      <ul
+      <div>{dropdown && dropdown['label']}</div>
+      <button
+        id="select-button"
         onKeyDown={handleKeypress}
-        id="dropdown-wrapper"
-        className={styleMod[dropdownWrapperClass]}
+        onClick={toggleDropdown}
       >
+        <span>{selectedItem ? selectedItem : 'Item 1'}</span>
+      </button>
+      <ul id="dropdown-wrapper" className={styleMod[dropdownWrapperClass]}>
         {dropdown &&
           dropdown.items.map((item: DropDownContentItemProps) => {
             return (
@@ -129,7 +197,6 @@ export const Component = (props: DropdownMenuDefaultProps) => {
                 id={`list-item-${item.id}`}
                 onClick={selectItem}
                 key={item.id}
-                onKeyDown={handleKeypress}
               >
                 {item.label}
               </li>
