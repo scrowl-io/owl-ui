18aaa04e6aed86968bd3cef5278150528da0f3ff
Valerie Wireko Osei <valeriew@SML391TO.local>
2022-05-25 16:23:58 -0400

    removed hello test component, recommitting branch

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,6 +1,5 @@
 import * as Button from '@owlui/button';
 import * as Card from '@owlui/card';
-import * as Hello from '@owlui/hello';
 import * as Icons from '@owlui/icons';
 import * as Navigationdrawer from '@owlui/navigationdrawer';
 import * as Table from '@owlui/table';
@@ -10,7 +9,6 @@ import './_index.scss';
 
 export const button = Button;
 export const card = Card;
-export const hello = Hello;
 export const icons = Icons;
 export const navigationdrawer = Navigationdrawer;
 export const table = Table;
@@ -20,7 +18,6 @@ export const textfield = Textfield;
 export default {
   button,
   card,
-  hello,
   icons,
   navigationdrawer,
   table,
--- packages/hello/src/Planet/Planet.types.ts
+++
@@ -1,7 +0,0 @@
-import type { HelloDefaultProps } from '../Default/Default.types';
-
-export type HelloPlanetCommons = HelloDefaultProps & {
-  
-};
-
-export type HelloPlanetProps = Partial<HelloPlanetCommons>;
