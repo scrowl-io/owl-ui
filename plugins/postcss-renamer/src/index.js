7cb2213f136a14b5819a1387853260786015b6ef
aubeOSG <caube@osg.ca>
2022-06-22 11:17:25 -0400

    UPDATE [sc-1089] - custom plugin working
    - adding the `owlui-` prefix to the `generateScopedName` causes a conflict with the custom module.

--- plugins/postcss-renamer/src/index.js
+++ plugins/postcss-renamer/src/index.js
@@ -2,60 +2,44 @@
 const parser = require('postcss-selector-parser');
 
 module.exports = (opts = {}) => {
-  const { classNames } = opts;
-  const classLookups = Object.keys(classNames);
-  const classLookupsLn = classLookups.length;
+  const { prefix, ignore } = opts;
 
-  const rename = selector => {
-    // eslint-disable-next-line no-prototype-builtins
-    if (classNames.hasOwnProperty(selector.value)) {
-      selector.value = classNames[selector.value];
+  if (typeof prefix !== 'string') {
+    throw new Error('@postcss-renamer: prefix option needs to be a string');
+  }
+
+  if (prefix.length <= 0) {
+    throw new Error('@postcss-renamer: prefix option needs to have a length');
+  }
+
+  if (ignore && !Array.isArray(ignore)) {
+    throw new Error('@postcss-renamer: ignore option needs to be an array');
+  }
+
+  const ignoreLn = Array.isArray(ignore) ? ignore.length : 0;
+
+  const isSelectorValid = selector => {
+    return (
+      (selector.type === 'class' || selector.type === 'id') &&
+      (ignoreLn > 0 ? ignore.indexOf(selector.value) === -1 : true)
+    );
+  };
+
+  const renamer = selector => {
+    if (isSelectorValid(selector)) {
+      selector.value = `${prefix}${selector.value}`;
     }
   };
 
-  const processor = parser(selectors => {
-    selectors.walkClasses(rename);
-    const renamed = selectors.nodes[0].nodes
-      .map(c => (c.type === 'class' ? `.${c.value}` : c.value))
-      .join('');
-
-    return renamed;
+  const transformer = parser(selectors => {
+    selectors.walkClasses(renamer);
   });
-
-  const getIndex = selector => {
-    let idx = -1;
-
-    for (let i = 0; i < classLookupsLn; i++) {
-      if (selector.indexOf(classLookups[i]) !== -1) {
-        idx = i;
-        return;
-      }
-    }
-
-    return idx;
-  };
-
-  const isRuleValid = rule => {
-    return getIndex(rule.selector) !== -1;
-  };
-
-  const updateRule = async rule => {
-    if (isRuleValid(rule)) {
-      const newClassName = await processor.process(rule);
-
-      rule.nodes[0].parent.selector = newClassName;
-      console.log('renamed rule', rule.selector);
-    }
-  };
 
   return {
     postcssPlugin: '@owlui/postcss-renamer',
-    async Rule(rule) {
-      updateRule(rule);
-    },
-    OnceExit(root, { result }) {
-      root.walkRules(rule => {
-        updateRule(rule);
+    OnceExit(root) {
+      root.walkRules(async rule => {
+        await transformer.process(rule);
       });
     },
   };
