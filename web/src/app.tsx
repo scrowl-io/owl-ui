504cf87641047202854874367b0fa2764f499da3
aubeOSG <caube@osg.ca>
2022-06-27 10:48:23 -0400

    UPDATE [sc-1127] [sc-1089] - minifing css post bundle

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -3,16 +3,16 @@ import { Button, Icon } from '@owlui/lib';
 
 const Page = () => {
   return (
-    <div className="owlui-grid-container">
-      <div className="owlui-grid-row">
-        <div className="owlui-grid-col-xs-4">
+    <div className="owlui-container">
+      <div className="owlui-row">
+        <div className="owlui-col-4">
           <Button>
             <Icon icon="settings" />
             Hello
           </Button>
         </div>
-        <div className="owlui-grid-col-xs-4">4</div>
-        <div className="owlui-grid-col-xs-4">4</div>
+        <div className="owlui-col-4">4</div>
+        <div className="owlui-col-4">4</div>
       </div>
     </div>
   );
