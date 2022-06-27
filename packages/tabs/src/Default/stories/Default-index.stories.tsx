56a8a45adf7b91aac484dcaee01055301e1633f0
cicerofonsecaosg <cicero@osg.ca>
2022-06-27 14:23:28 -0400

    sc-890 Update component import in story

--- packages/tabs/src/Default/stories/Default-index.stories.tsx
+++ packages/tabs/src/Default/stories/Default-index.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { component as Tabs } from '../index';
+import { Tabs } from '../index';
 import { TabsDefaultProps } from '../Default.types';
 
 import { appearance } from './Default-appearance.stories';
