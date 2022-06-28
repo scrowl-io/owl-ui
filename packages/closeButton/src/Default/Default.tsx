76953a417d7063ee7fd5c84743c67e8819bfd196
cicerofonsecaosg <cicero@osg.ca>
2022-06-28 13:17:23 -0400

    sc-898 Clean up component and update types

--- packages/closeButton/src/Default/Default.tsx
+++ packages/closeButton/src/Default/Default.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react';
-import { ThemeProvider, CloseButton } from 'react-bootstrap';
-import { CloseButtonDefaultProps, themePrefixesProps } from './Default.types';
+import React from 'react';
+import { CloseButton } from 'react-bootstrap';
+import { CloseButtonDefaultProps } from './Default.types';
 import * as styleMod from './styles.module.scss';
 import { createLocalProps } from '@owlui/utils';
 
 export const Component = (props: CloseButtonDefaultProps) => {
-  const themePrefixes: themePrefixesProps = {};
   const baseClass = 'btn-close';
-  const { children } = props;
   const prefix = props.prefix || '';
 
   const locals = createLocalProps(
@@ -17,39 +15,13 @@ export const Component = (props: CloseButtonDefaultProps) => {
       classes: {
         base: baseClass,
         prefix: prefix,
-        optionals: [
-          {
-            fields: ['theme'],
-            value: 'theme',
-          },
-          {
-            fields: ['size'],
-            value: 'Size',
-          },
-        ],
+        optionals: [],
       },
     },
-    ['prefix', 'theme', 'size']
+    ['prefix']
   );
 
-  themePrefixes[baseClass] = `owlui-${baseClass}`;
-
-  useEffect(() => {
-    const closebutton: HTMLElement | null =
-      document.querySelector('.btn-close');
-
-    if (closebutton instanceof Element === false) {
-      return;
-    }
-
-    closebutton?.classList.add('owlui-btn-close');
-  });
-
-  return (
-    <ThemeProvider prefixes={themePrefixes}>
-      <CloseButton {...locals}>{children}</CloseButton>
-    </ThemeProvider>
-  );
+  return <CloseButton {...locals} />;
 };
 
 export default {
--- packages/closeButton/src/Default/Default.types.ts
+++ packages/closeButton/src/Default/Default.types.ts
@@ -1,16 +1,9 @@
-import React from 'react';
 import * as bs from 'react-bootstrap';
 
 export interface CloseButtonDefaultCommons {
-  theme?: 'Default' | 'Dark';
   size?: 'Sm' | 'Md' | 'Lg';
   prefix?: string;
-  disabled: boolean;
 }
 
-export interface themePrefixesProps {
-  [key: string]: string;
-}
-
-export type CloseButtonDefaultProps = Partial<CloseButtonDefaultCommons> &
+export type CloseButtonDefaultProps = CloseButtonDefaultCommons &
   bs.CloseButtonProps;
