268d1f8e770a775258712da01a6998a5f5f6e6e9
aubeOSG <caube@osg.ca>
2022-06-29 09:39:49 -0400

    UPDATE [sc-1151] - added link option for nav drawer

--- packages/navigationDrawer/src/Default/elements/Item.tsx
+++ packages/navigationDrawer/src/Default/elements/Item.tsx
@@ -2,12 +2,20 @@ import * as React from 'react';
 import { NavigationDrawerContentItemProps } from '../Default.types';
 import * as styles from '../styles.module.scss';
 
-const baseClass = 'navigationDrawerItem';
-
+const baseClass = 'owluiNavigationDrawerItem';
+const linkClass = `${baseClass}Link`;
+console.log('styles', styles);
 export const Component = (props: NavigationDrawerContentItemProps) => {
-  const { label } = props;
+  const { label, url } = props;
+  const content = !url ? (
+    label
+  ) : (
+    <a className={styles[linkClass]} href={url}>
+      {label}
+    </a>
+  );
 
-  return <div className={styles[baseClass]}>{label}</div>;
+  return <div className={styles[baseClass]}>{content}</div>;
 };
 
 export default {
