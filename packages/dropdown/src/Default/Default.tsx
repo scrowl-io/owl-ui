c69fe1ceb441a070feef6a6e50447ff9e046fa93
aubeOSG <caube@osg.ca>
2022-09-28 13:11:37 -0400

    UPDATE - prop not related to BS dropdown

--- packages/dropdown/src/Default/Default.tsx
+++ packages/dropdown/src/Default/Default.tsx
@@ -29,7 +29,7 @@ export const Dropdown = (props: DropdownDefaultProps) => {
         ],
       },
     },
-    ['prefix', 'theme', 'pxScale', 'items', 'button']
+    ['prefix', 'theme', 'pxScale', 'items', 'button', 'showSelected']
   );
 
   const getItemIdx = (
