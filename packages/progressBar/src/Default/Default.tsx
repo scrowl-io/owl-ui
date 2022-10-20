0ed4443a2d4c159d704f6aa79ec533cbfd7291cf
aubeOSG <caube@osg.ca>
2022-10-20 12:38:27 -0400

    UPDATE - applied lookup util

--- packages/progressBar/src/Default/Default.tsx
+++ packages/progressBar/src/Default/Default.tsx
@@ -2,47 +2,41 @@ import React from 'react';
 import { ThemeProvider, ProgressBar as BsProgressBar } from 'react-bootstrap';
 import { ProgressBarDefaultProps } from './Default.types';
 import * as styleMod from './styles.module.scss';
-import {
-  createLocalProps,
-  themePrefixesProps,
-} from '../../../../lib/src/utils';
+import { getClasses, themePrefixesProps } from '../../../../lib/src/utils';
 
-export const ProgressBar = (props: ProgressBarDefaultProps) => {
+export const ProgressBar = ({
+  className,
+  pxScale,
+  now,
+  showLabel,
+  ...props
+}: ProgressBarDefaultProps) => {
   const themePrefixes: themePrefixesProps = {};
   const baseClass = 'progress';
-  const { now, variant, showLabel } = props;
-  const prefix = props.prefix || '';
-
-  const locals = createLocalProps(
-    props,
-    {
-      module: styleMod,
-      classes: {
-        base: baseClass,
-        prefix: prefix,
-        optionals: [
-          {
-            fields: ['theme'],
-            value: 'theme',
-          },
-          {
-            fields: ['pxScale'],
-            value: 'PxScale',
-          },
-        ],
+  let classes = getClasses({
+    module: styleMod,
+    base: baseClass,
+    modifiers: [
+      {
+        base: 'PxScale',
+        value: pxScale,
       },
-    },
-    ['prefix', 'theme', 'pxScale', 'showLabel']
-  );
+    ],
+  });
 
   themePrefixes[baseClass] = `owlui-${baseClass}`;
   themePrefixes[`${baseClass}-bar`] = `owlui-${baseClass}-bar`;
 
+  if (className) {
+    classes += ` ${className}`;
+  }
+
   return (
     <ThemeProvider prefixes={themePrefixes}>
       <BsProgressBar
-        {...locals}
-        variant={variant}
+        className={classes}
+        {...props}
+        now={now}
         label={showLabel ? `${now}%` : ''}
         style={showLabel ? { height: 'auto' } : undefined}
       />
