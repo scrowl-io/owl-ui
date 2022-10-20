0a355cd8f71e124a58d7b150d59fc5e38558e612
aubeOSG <caube@osg.ca>
2022-10-20 11:41:59 -0400

    UPDATE - applied lookup util

--- packages/closeButton/src/Default/Default.tsx
+++ packages/closeButton/src/Default/Default.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import { CloseButton as BsCloseButton } from 'react-bootstrap';
+import { CloseButton as BsCloseButton, ThemeProvider } from 'react-bootstrap';
 import { CloseButtonDefaultProps } from './Default.types';
 import * as styleMod from './styles.module.scss';
-import { createLocalProps } from '../../../../lib/src/utils';
+import { getClasses, themePrefixesProps } from '../../../../lib/src/utils';
 
-export const CloseButton = (props: CloseButtonDefaultProps) => {
+export const CloseButton = ({
+  className,
+  pxScale,
+  ...props
+}: CloseButtonDefaultProps) => {
+  const themePrefixes: themePrefixesProps = {};
   const baseClass = 'btn-close';
-  const prefix = props.prefix || '';
-
-  const locals = createLocalProps(
-    props,
-    {
-      module: styleMod,
-      classes: {
-        base: baseClass,
-        prefix: prefix,
-        optionals: [],
+  let classes = getClasses({
+    module: styleMod,
+    base: baseClass,
+    modifiers: [
+      {
+        base: 'PxScale',
+        value: pxScale,
       },
-    },
-    ['prefix']
-  );
+    ],
+  });
 
-  return <BsCloseButton {...locals} />;
+  themePrefixes[baseClass] = `owlui-${baseClass}`;
+
+  if (className) {
+    classes += ` ${className}`;
+  }
+
+  return (
+    <ThemeProvider prefixes={themePrefixes}>
+      <BsCloseButton className={classes} {...props} />
+    </ThemeProvider>
+  );
 };
 
 export default {
