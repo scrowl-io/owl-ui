b31a3d6c07a13cb17c1dd3a5dd127612f6a57ab9
aubeOSG <caube@osg.ca>
2022-10-20 12:19:31 -0400

    UPDATE - applied lookup util

--- packages/modal/src/Default/Default.tsx
+++ packages/modal/src/Default/Default.tsx
@@ -2,34 +2,28 @@ import * as React from 'react';
 import { ThemeProvider, Modal as BsModal } from 'react-bootstrap';
 import { ModalDefaultProps } from './Default.types';
 import * as styleMod from './styles.module.scss';
-import {
-  createLocalProps,
-  themePrefixesProps,
-} from '../../../../lib/src/utils';
+import { getClasses, themePrefixesProps } from '../../../../lib/src/utils';
 
-export const Modal = (props: ModalDefaultProps) => {
+export const Modal = ({
+  className,
+  header,
+  pxScale,
+  body,
+  footer,
+  ...props
+}: ModalDefaultProps) => {
   const themePrefixes: themePrefixesProps = {};
   const baseClass = 'modal';
-  const { header, body, footer } = props;
-  const prefix = props.prefix || '';
-
-  const locals = createLocalProps(
-    props,
-    {
-      module: styleMod,
-      classes: {
-        base: baseClass,
-        prefix: prefix,
-        optionals: [
-          {
-            fields: ['theme'],
-            value: 'theme',
-          },
-        ],
+  let classes = getClasses({
+    module: styleMod,
+    base: baseClass,
+    modifiers: [
+      {
+        base: 'PxScale',
+        value: pxScale,
       },
-    },
-    ['prefix', 'theme', 'modalContent']
-  );
+    ],
+  });
 
   themePrefixes[baseClass] = `owlui-${baseClass}`;
   themePrefixes['modal-body'] = 'owlui-modal-body';
@@ -38,9 +32,13 @@ export const Modal = (props: ModalDefaultProps) => {
   themePrefixes['modal-content'] = 'owlui-modal-content';
   themePrefixes['modal-title'] = 'owlui-modal-title';
 
+  if (className) {
+    classes += ` ${className}`;
+  }
+
   return (
     <ThemeProvider prefixes={themePrefixes}>
-      <BsModal {...locals}>
+      <BsModal className={classes} {...props}>
         {header ? (
           <BsModal.Header {...header.bsProps}>
             <BsModal.Title>{header.content}</BsModal.Title>
