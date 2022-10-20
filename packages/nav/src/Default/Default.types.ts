dfdfc5d29e531d932bf2292fa92441296f6723a7
aubeOSG <caube@osg.ca>
2022-10-20 12:22:47 -0400

    Update Default.types.ts

--- packages/nav/src/Default/Default.types.ts
+++ packages/nav/src/Default/Default.types.ts
@@ -3,6 +3,7 @@ import * as bs from 'react-bootstrap';
 export interface NavDefaultCommons {
   theme?: 'Default' | 'Dark';
   prefix?: string;
+  pxScale?: 'Sm' | 'Md' | 'Lg';
 }
 
 export type NavDefaultProps = NavDefaultCommons & bs.NavProps;
