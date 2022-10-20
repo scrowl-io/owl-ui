c11f7ca3449f8286e8682ee70b3ddac8274c4643
aubeOSG <caube@osg.ca>
2022-10-20 13:37:57 -0400

    UPDATE - applied lookup util

--- packages/spinner/src/Default/Default.tsx
+++ packages/spinner/src/Default/Default.tsx
@@ -2,44 +2,36 @@ import * as React from 'react';
 import { ThemeProvider, Spinner as BsSpinner } from 'react-bootstrap';
 import { SpinnerDefaultProps } from './Default.types';
 import * as styleMod from './styles.module.scss';
-import {
-  createLocalProps,
-  themePrefixesProps,
-} from '../../../../lib/src/utils';
+import { getClasses, themePrefixesProps } from '../../../../lib/src/utils';
 
-export const Spinner = (props: SpinnerDefaultProps) => {
+export const Spinner = ({
+  className,
+  pxScale,
+  animation,
+  ...props
+}: SpinnerDefaultProps) => {
   const themePrefixes: themePrefixesProps = {};
   const baseClass = 'spinner';
-  const { animation } = props;
-  const prefix = props.prefix || '';
-
-  const locals = createLocalProps(
-    props,
-    {
-      module: styleMod,
-      classes: {
-        base: baseClass,
-        prefix: prefix,
-        optionals: [
-          {
-            fields: ['theme'],
-            value: 'theme',
-          },
-          {
-            fields: ['pxScale'],
-            value: 'PxScale',
-          },
-        ],
+  let classes = getClasses({
+    module: styleMod,
+    base: baseClass,
+    modifiers: [
+      {
+        base: 'PxScale',
+        value: pxScale,
       },
-    },
-    ['prefix', 'theme', 'pxScale']
-  );
+    ],
+  });
 
   themePrefixes[baseClass] = `owlui-${baseClass}`;
 
+  if (className) {
+    classes += ` ${className}`;
+  }
+
   return (
     <ThemeProvider prefixes={themePrefixes}>
-      <BsSpinner animation={animation} {...locals} />
+      <BsSpinner animation={animation} className={classes} {...props} />
     </ThemeProvider>
   );
 };
