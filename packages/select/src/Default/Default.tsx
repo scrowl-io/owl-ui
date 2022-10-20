318a5b2e4782d472cd4934a80024d38ceb2e0856
aubeOSG <caube@osg.ca>
2022-10-20 13:15:43 -0400

    UPDATE - applied lookup util

--- packages/select/src/Default/Default.tsx
+++ packages/select/src/Default/Default.tsx
@@ -2,46 +2,41 @@ import React from 'react';
 import { ThemeProvider, Form } from 'react-bootstrap';
 import { SelectDefaultProps } from './Default.types';
 import * as styleMod from './styles.module.scss';
-import {
-  createLocalProps,
-  themePrefixesProps,
-} from '../../../../lib/src/utils';
+import { getClasses, themePrefixesProps } from '../../../../lib/src/utils';
 
-export const Select = (props: SelectDefaultProps) => {
+export const Select = ({
+  className,
+  pxScale,
+  label,
+  control,
+  ...props
+}: SelectDefaultProps) => {
   const themePrefixes: themePrefixesProps = {};
   const baseClass = 'select';
-  const { label, control } = props.inputProps;
-  const prefix = props.prefix || '';
-
-  const locals = createLocalProps(
-    props,
-    {
-      module: styleMod,
-      classes: {
-        base: baseClass,
-        prefix: prefix,
-        optionals: [
-          {
-            fields: ['theme'],
-            value: 'theme',
-          },
-          {
-            fields: ['pxScale'],
-            value: 'PxScale',
-          },
-        ],
+  let classes = getClasses({
+    module: styleMod,
+    base: baseClass,
+    modifiers: [
+      {
+        base: 'PxScale',
+        value: pxScale,
       },
-    },
-    ['prefix', 'theme', 'pxScale', 'inputProps']
-  );
+    ],
+  });
 
   themePrefixes[baseClass] = `owlui-${baseClass}`;
   themePrefixes['form-label'] = 'owlui-form-label';
   themePrefixes['form-select'] = 'owlui-form-select';
 
+  if (className) {
+    classes += ` ${className}`;
+  }
+
+  classes += 'mb-3';
+
   return (
     <ThemeProvider prefixes={themePrefixes}>
-      <Form.Group {...locals} className="mb-3">
+      <Form.Group className={classes} {...props}>
         <Form.Label {...label}>{label?.content}</Form.Label>
         <Form.Select {...control}>
           {control.options.map(option => {
--- packages/select/src/Default/Default.types.ts
+++ packages/select/src/Default/Default.types.ts
@@ -2,24 +2,18 @@
 /* eslint-disable import/namespace */
 import React from 'react';
 import * as bs from 'react-bootstrap';
+
 export interface SelectDefaultCommons {
   theme?: 'Default' | 'Dark';
   pxScale?: 'Sm' | 'Md' | 'Lg';
   prefix?: string;
   type?: string;
-  inputProps: SelectProps;
-}
-export interface SelectProps {
-  id?: string;
-  label: SelectLabel & bs.FormLabelProps;
-  control: SelectOptions;
-}
-export interface SelectLabel {
-  content: string;
+  label: bs.FormLabelProps & {
+    content: string;
+  };
+  control: {
+    options: React.OptionHTMLAttributes<HTMLOptionElement>[];
+  };
 }
 
-export interface SelectOptions extends bs.FormSelectProps {
-  options: React.OptionHTMLAttributes<HTMLOptionElement>[];
-}
-
-export type SelectDefaultProps = SelectDefaultCommons;
+export type SelectDefaultProps = SelectDefaultCommons & bs.FormSelectProps;
